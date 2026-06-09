040a40e9ac14237da72c6dc72092d1940f378c4b
njayinthehouse <nitinjohnraj@gmail.com>
2026-06-09 04:36:52 +0000

    [zsh] Common aliases and dotfile management

---
+++ .config/nvim/init.lua
@@ -0,0 +1,21 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local path = vim.api.nvim_buf_get_name(0)
+    if vim.fn.isdirectory(path) == 1 then
+      vim.cmd.cd(path)          -- cwd becomes the directory you passed
+      vim.cmd.terminal()        -- replaces the dir buffer with a terminal
+      vim.cmd.startinsert()     -- drop straight into terminal insert mode
+    end
+  end,
+})
+
+vim.keymap.set('x', '<C-S-c>', '"+y')                 -- copy visual selection
+vim.keymap.set('n', '<C-S-v>', '"+p')                 -- paste (normal)
+vim.keymap.set('i', '<C-S-v>', '<C-r><C-o>+')         -- paste (insert, literal)
+vim.keymap.set('c', '<C-S-v>', '<C-r>+')              -- paste (cmdline)
+vim.keymap.set('t', '<C-S-v>', function()             -- paste into a :ter's shell
+    vim.fn.chansend(vim.b.terminal_job_id, vim.fn.getreg('+'))
+  end)
---
+++ .zshrc
@@ -0,0 +1,12 @@
+alias l='ls'
+alias la='ls -a'
+alias ll='ls -l'
+alias lla='ls -l -a'
+alias zshconf='nvim ~/.zsh'
+alias nvimconf='nvim ~/.conf/nvim/init.lua'
+alias vim='nvim'
+
+# Version control for dotfiles
+alias dot='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+# export PATH="$PATH"
